e7651ef3eef36f8ad82e9b2eaad251a62714b775
bjslagt <bjslagt@gmail.com>
2020-06-02 11:38:58 +0300

    Updated it with more comments and removed the 'cached' print statement on the console

--- cachematrix.R
+++ cachematrix.R
@@ -7,13 +7,23 @@
 ## If the cache hasn't been set yet, a getinverse will give NULL
 ## COMMENT ENDS -- BJSLAGT
 makeCacheMatrix <- function(x = matrix()) {
+
+  #cached inverse to NULL
   inv <- NULL
+  
   set <- function(y){
+    #if we set a new matrix, we need to NULL the cache
     x <<- y
     inv <<- NULL
   }
+
+  #get function to retrieve matrix
   get <- function() x
+  
+  #set function to set the inverse matrix
   setinverse <- function(i) inv<<- i
+  
+  #get function to get the inverse matrix
   getinverse <- function() inv
   list(set = set, get = get , setinverse = setinverse, getinverse = getinverse)
 }
@@ -28,12 +38,27 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  
+  #get the cached inverse (if existing)
   inv <- x$getinverse()
+  
+  #if it didn't exist the inverse was NULL
   if(!is.null(inv)){
+    #print("Return CACHED version")
+    #removed the print statement to not provide unneccesary output on the console
+    
     return(inv)
   }
+  
+  #get original matrix
   data<-x$get()
-  inv<-solve(x)
+  
+  #calculate inverse using solve()
+  inv<-solve(data, ...)
+  
+  #then set the cache
   x$setinverse(inv)
+  
+  #and return the inverse
   inv
 }
